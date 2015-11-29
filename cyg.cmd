3ff00eb6c8e00e6d61fd7da0295e975da522307e
QGB <qgbcs1@gmail.com>
2015-11-29 12:01:11 +0800

    2003

--- cyg.cmd
+++ cyg.cmd
@@ -1,14 +1,1 @@
-@set path=%path%
-@set path=%path%
-@set path=D:\Program Files\Graphviz2.38\bin;%path%
-@set path=C:\HP\bin;%path%
-@set path=C:\Python27;%path%
-@set path=C:\Python27\Scripts;%path%
-@set path=D:\Cygwin\bin;%path%
-@set path=D:\Program Files\CodeBlocks\MinGW\bin;%path%
-@set path=D:\Program Files\Git\bin;%path%
-@set path=D:\Program Files\Git\mingw32\bin;%path%
-@set path=D:\Program Files\Git\usr\bin;%path%
-@set path=D:\Program Files\Java\jdk1.8.0_45\bin;%path%
-@set path=D:\Program Files\nodejs;%path%
-@set path=E:\SourceCode\shell;%path%
+@"E:\SourceCode\shell\cyg.cmd"
---
+++ gqgb.bat
@@ -0,0 +1,2 @@
+git config --global user.email qgbcs1@gmail.com
+git config --global user.name QGB
